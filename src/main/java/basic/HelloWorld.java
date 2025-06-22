ef43e67474c6ec925b9cab6a92b0992fc5aa1529
Gatla Lakshman <lakhi.gemini@gmail.com>
2025-06-22 20:01:33 +0800

    added b1 related codes

--- src/main/java/basic/HelloWorld.java
+++ src/main/java/basic/HelloWorld.java
@@ -3,8 +3,12 @@ package basic;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hellow World");		
-
+		System.out.println("Hellow World");
 	}
-
+	
+	//from b1 branch , in real time it could be a bug fix or new fix or modification or deletion of the feature( dx )
+	public void m1() {
+		System.out.println("from method-1");
+	}
+	//code change can be one or more files 
 }
